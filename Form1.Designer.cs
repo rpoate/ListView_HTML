6d80e4574ab44950ac4f2c300fe188149ff653fd
Richard Poate <info@zoople.com.au>
2021-10-28 21:48:33 +1100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿
+namespace ListHTML
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.htmlEditControl1 = new Zoople.HTMLEditControl();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.listView1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.htmlEditControl1);
+            this.splitContainer1.Size = new System.Drawing.Size(890, 503);
+            this.splitContainer1.SplitterDistance = 190;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // listView1
+            // 
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(190, 503);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // htmlEditControl1
+            // 
+            this.htmlEditControl1.AcceptsReturn = true;
+            this.htmlEditControl1.AllowDragInternal = true;
+            this.htmlEditControl1.AvailableInlineStyles = ((System.Collections.Generic.List<string>)(resources.GetObject("htmlEditControl1.AvailableInlineStyles")));
+            this.htmlEditControl1.BaseURL = null;
+            this.htmlEditControl1.CleanMSWordHTMLOnPaste = true;
+            this.htmlEditControl1.CodeEditor.Enabled = true;
+            this.htmlEditControl1.CodeEditor.Locked = false;
+            this.htmlEditControl1.CodeEditor.TabWidth = 720;
+            this.htmlEditControl1.CodeEditor.WordWrap = false;
+            this.htmlEditControl1.CSSText = null;
+            this.htmlEditControl1.CustomColorPalette = null;
+            this.htmlEditControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.htmlEditControl1.DocumentHTML = null;
+            this.htmlEditControl1.EditingDisabled = false;
+            this.htmlEditControl1.EnableInlineSpelling = false;
+            this.htmlEditControl1.FontSizesList = null;
+            this.htmlEditControl1.FontsList = null;
+            this.htmlEditControl1.HiddenButtons = null;
+            this.htmlEditControl1.ImageStorageLocation = null;
+            this.htmlEditControl1.InCodeView = false;
+            this.htmlEditControl1.LanguageFile = null;
+            this.htmlEditControl1.LicenceActivationKey = null;
+            this.htmlEditControl1.LicenceKey = null;
+            this.htmlEditControl1.LicenceKeyInlineSpelling = null;
+            this.htmlEditControl1.Location = new System.Drawing.Point(0, 0);
+            this.htmlEditControl1.Name = "htmlEditControl1";
+            this.htmlEditControl1.Size = new System.Drawing.Size(696, 503);
+            this.htmlEditControl1.SpellingAutoCorrectionList = ((System.Collections.Hashtable)(resources.GetObject("htmlEditControl1.SpellingAutoCorrectionList")));
+            this.htmlEditControl1.TabIndex = 0;
+            this.htmlEditControl1.ToolstripImageScalingSize = new System.Drawing.Size(16, 16);
+            this.htmlEditControl1.UseParagraphAsDefault = true;
+            this.htmlEditControl1.HTMLChanged += new Zoople.HTMLEditControl.HTMLChangedEventHandler(this.htmlEditControl1_HTMLChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(890, 503);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private Zoople.HTMLEditControl htmlEditControl1;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ListHTML
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.htmlEditControl1.CSSText = "body {font-family: Arial;}";
+
+            this.listView1.Items.Add("test1").Tag = "<p>test1</p>";
+            this.listView1.Items.Add("test2").Tag = "<p>test12345</p>";
+            this.listView1.Items.Add("test3").Tag = "<p>Another Test</p>";
+            this.listView1.Items.Add("test4").Tag = "<h1>Heading 1 Test</h1>";
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (this.listView1.SelectedItems.Count == 0)
+                return;
+
+            this.htmlEditControl1.DocumentHTML = listView1.SelectedItems[0].Tag.ToString();
+        }
+
+        private void htmlEditControl1_HTMLChanged(object sender, EventArgs e)
+        {
+            if (this.listView1.SelectedItems.Count == 0)
+                return;
+
+            listView1.SelectedItems[0].Tag = this.htmlEditControl1.DocumentHTML;
+        }
+    }
+}
